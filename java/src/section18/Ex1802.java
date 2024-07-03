1351f70138f454dd39e279cda3d229b09b3a002b
bbangbeom <bbangbeom81@gmail.com>
2024-07-03 21:47:11 +0900

    복습 + section18 입출력

---
+++ java/src/section18/Ex1802.java
@@ -0,0 +1,10 @@
+package section18;
+
+public class Ex1802 {
+    public static void main(String[] args) {
+        System.out.print("java is \n"); // \n 개행 문자
+        System.out.print("nice! \n");
+        System.out.println("java is"); 
+        System.out.println("nice!");
+    }
+}
---
+++ java/src/section18/Ex1803.java
@@ -0,0 +1,22 @@
+package section18;
+
+public class Ex1803 {
+        // 서식 문자
+        public static void main(String[] args) {
+        System.out.println(String.format("I eat %d apples.", 5));
+
+        System.out.printf("I eat %d apples.\n", 5);
+        System.out.printf("I eat %f apples.\n", 2.5);
+        System.out.printf("I eat %.2f apples.\n", 2.5);
+        System.out.printf("I eat %s apples.\n", "five");
+        System.out.printf("I eat %s apples.\n", 2.50);
+
+        System.out.printf("I have completed %d%% of my Java studies.\n", 100);
+        System.out.printf("I have completed %d%% of my %s studies.\n", 100, "Java");
+        System.out.printf("I have completed %3d%% of my %s studies.\n", 90, "Java");
+        System.out.printf("I have completed %-3d%% of my %s studies.\n", 90, "Java");
+        System.out.printf("I have completed %.2f%% of my %s studies.\n", 99.9999, "Java");
+        System.out.printf("I have completed %s%% of my %s studies.\n", "99.9999", "Java");
+    }
+    
+}
